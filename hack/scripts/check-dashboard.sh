50bd13efbbc7ede8436abf2c6291c8a93ce16d7c
sayedppqq <sayed@appscode.com>
2024-05-14 11:21:15 +0600

    wip
    Signed-off-by: sayedppqq <sayed@appscode.com>

---
+++ hack/scripts/check-dashboard.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eou pipefail
+DB=$1
+allDbs=("MongoDB" "ConnectCluster" "Druid" "Elasticsearch" "Kafka" )
+if [ $(DB)=="all" ]; then
+    kubectl apply -f https://raw.githubusercontent.com/kubedb/samples/old-dbs/mongodb/shard/mongodb.yaml
+    kubectl wait --for=jsonpath='{.status.phase}'=Ready MongoDB mongodb -n demo --timeout=10m
+    kubectl-dba monitor dashboard mongodb mongodb-summary-dashboard --prom-svc-name=prometheus-kube-prometheus-prometheus --prom-svc-namespace=monitoring --prom-svc-port=9090
+fi
